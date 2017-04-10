00df13b80a9495e06e1c7233bc024ae25e2238e0
zaporylie <jakub@nymedia.no>
2017-04-10 08:29:53 +0200

    Working installation procedure

--- src/DrupalInstaller.php
+++ src/DrupalInstaller.php
@@ -2,7 +2,74 @@
 
 namespace zaporylie\DrupalInstaller;
 
-class DrupalInstaller
-{
+use Composer\IO\IOInterface;
+use Composer\Script\Event;
+
+class DrupalInstaller {
+
+  /**
+   * Interactively install Drupal.
+   *
+   * @param \Composer\Script\Event $event
+   */
+  public static function installDrupal(Event $event) {
+    $io = $event->getIO();
+
+    $drupalFinder = new \DrupalFinder\DrupalFinder();
+    if (!$drupalFinder->locateRoot(getcwd())) {
+      $io->writeError('<error>Unable to locate Drupal root</error>');
+      return;
+    }
+    $drupalRoot = $drupalFinder->getDrupalRoot();
+    $io->write("<info>Drupal root: $drupalRoot</info>", true, IOInterface::VERBOSE);
+
+    // Get confirmation.
+    $confirmation = $io->askConfirmation('<info>Do you want to install Drupal?</info> [<comment>Y,n</comment>]',TRUE);
+    if (!$confirmation) {
+      $io->write('<warning>Drupal installation has been skipped</warning>');
+      return;
+    }
+
+    // @todo Find available profiles and add to option list.
+    $profile = $io->select('<info>The install profile you wish to run.</info> [<comment>default</comment>]', ['default' => 'default'], 'default');
+    if ($profile == 'default') {
+      $profile = NULL;
+    }
+
+    // @todo: Find if settings.php exists and already has db credentials.
+    // Ask if stored credentials should be used.
+
+    // New database defaults.
+    $db_driver = $io->ask('<info>Database drriver:</info> [<comment>mysql</comment>] ', 'mysql');
+    $db_user = $io->askAndValidate('<info>Database user:</info> ', '\\zaporylie\\DrupalInstaller\\DrupalInstaller::isNotEmpty');
+    $db_password = $io->askAndValidate('<info>Database password:</info> ', '\\zaporylie\\DrupalInstaller\\DrupalInstaller::isNotEmpty');
+    $db_host = $io->ask('<info>Database host:</info> [<comment>localhost</comment>] ', 'localhost');
+    $db_port = $io->ask('<info>Database port:</info> [<comment>3306</comment>] ',  3306);
+
+    // Database name.
+    $db_name = $io->askAndValidate('<info>Database name:</info> ', '\\zaporylie\\DrupalInstaller\\DrupalInstaller::isNotEmpty');
+
+    // Sudo user.
+    $db_su_user = $io->ask('<info>Database admin user:</info> [<comment>root</comment>] ', 'root');
+    $db_su_password = $io->ask('<info>Database admin password:</info> ');
+
+    $output = [];
+    exec("composer exec -v -- drush --root=$drupalRoot si $profile --db-url='$db_driver://$db_user:$db_password@$db_host:$db_port/$db_name' --db-su='$db_su_user' --db-su-pw='$db_su_password' -y", $output);
+    foreach ($output as $line) {
+      $io->write($line);
+    }
+
+  }
+
+  /**
+   * @param $value
+   * @return mixed
+   */
+  public static function isNotEmpty($value) {
+    if (!empty($value)) {
+      return $value;
+    }
+    throw new \InvalidArgumentException('Input cannot be empty');
+  }
 
 }
